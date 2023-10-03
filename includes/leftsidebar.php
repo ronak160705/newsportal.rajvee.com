eaf24083e3e572d3eb224f28d00424a39396ff72
ronak160705 <126779592+ronak160705@users.noreply.github.com>
2023-10-03 21:51:19 +0530

    Add files via upload

---
+++ includes/leftsidebar.php
@@ -0,0 +1,44 @@
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                        	<li class="menu-title">Navigation</li>
+
+                            <li class="has_sub">
+                                <a href="adminpage.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                         
+                            </li>
+
+                            <li class="has_sub">
+                                <a href="fetch.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Users </span> </a>
+                         
+                            </li>
+
+                            <li class="has_sub">
+                                <a href="c.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Contact </span> </a>
+                         
+                            </li>
+
+                            <li class="has_sub">
+                                <a href="show_feedback.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Feedback </span> </a>
+                         
+                            </li>
+                            
+                            
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+                    <div class="help-box">
+                        <h5 class="text-muted m-t-0">For Help ?</h5>
+                        <p class=""><span class="text-custom">Email:</span> <br/> phpgurukulofficial@gmail.com</p>
+                    </div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
